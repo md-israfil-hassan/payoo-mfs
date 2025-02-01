c11a736333043c81be570311118ae04cd1df2368
md-israfil-hassan <mdisrafilhassan6619@gmail.com>
2025-02-02 01:13:59 +0600

    new balance added UI DOM

--- js/home.js
+++ js/home.js
@@ -3,6 +3,7 @@
  *  S-1 : add event Hnadler 
  * prevent page reload after form submit
  * S-2: get money to be added to the account balance
+ * S-3: verify the pin number
  */
 
 
@@ -19,6 +20,29 @@ const addMoneyInput = document.getElementById('input-add-money').value ;
 //get the pin number provide
 const pinNumberInput = document.getElementById('input-pin-number').value ;
 
-console.log(addMoneyInput, pinNumberInput)
+console.log(typeof pinNumberInput)
+
+// step: verify pin number
+// wrong way to validate pin number
+if( pinNumberInput === '1234'){
+    console.log('adding money to your account')
+    //step-4: get the current balcnce
+const balance = document.getElementById('account-balance').innerText;
+console.log(typeof balance) ;
+
+//step-5: add addmoney input with balance
+
+const addMoneyNumber = parseFloat(addMoneyInput) ;
+const balanceNumber = parseFloat(balance);
+const newBalance = addMoneyNumber + balanceNumber ;
+
+// step-6 : update the balance in the UI/DOM
+document.getElementById('account-balance').innerText=newBalance ;
+
+}
+
+else{
+    alert('faild to add money! please try again')
+}
 
 })
